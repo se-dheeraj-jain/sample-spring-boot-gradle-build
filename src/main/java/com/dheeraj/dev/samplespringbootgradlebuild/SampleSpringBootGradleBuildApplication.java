3cdffb798b00163e2ef0816aa1e76ffb53fcc8fe
Dheeraj <dheerajjain004@gmail.com>
2024-05-25 13:48:46 +0100

    init sample project

---
+++ src/main/java/com/dheeraj/dev/samplespringbootgradlebuild/SampleSpringBootGradleBuildApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.dev.samplespringbootgradlebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootGradleBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootGradleBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dheeraj/dev/samplespringbootgradlebuild/SampleSpringBootGradleBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeraj.dev.samplespringbootgradlebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootGradleBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
